c59d8634d3a33157f67bd619d88a87e5ac80c09f
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-01-31 15:05:38 -0500

    Calcular area de un circulo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
-
+    var radio = 0.0
+    var PI= 3.1415
+    tituloPrincipal()
+    ingresarRadio()
+    radio = readln().toDouble()
+    println("El area del circulo es ${calcularArea(radio = radio, PI = PI )}")
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,9 @@
+fun tituloPrincipal(){
+    println("Calcular el area de un circulo")
+}
+fun ingresarRadio(){
+    println("Ingrese el radio:")
+}
+fun calcularArea(radio: Double, PI: Double):Double{
+     return PI*(radio*radio)
+}
