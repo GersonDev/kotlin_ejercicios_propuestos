5066e38151a59d2e9fa0569f5b80898691d88929
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-02-02 11:00:40 -0500

    ejercicio_5_dado_2_numeros_calcular_cuanto_numeros_estan_entre_ellos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-
+    var primerNumero = 0
+    var segundoNumero = 0
+    tituloPrincipal()
+    tituloPrimerNumero()
+    primerNumero = readln().toInt()
+    tituloSegundoNumero()
+    segundoNumero = readln().toInt()
+    println("Los numero que hay entre el $primerNumero y el $segundoNumero son ->  ${calcularCantidadDeNumeros(primerNumero = primerNumero, segundoNumero = segundoNumero)}")
 }
--- src/main/kotlin/Utilitarios.kt
+++ src/main/kotlin/Utilitarios.kt
@@ -10,12 +10,13 @@ fun tituloSegundoNumero() {
     println("Ingrese segundo numero:")
 }
 
-fun calcularCantidadDeNumeros(primerNumero: Int, segundoNumero: Int):Int {
+fun calcularCantidadDeNumeros(primerNumero: Int, segundoNumero: Int): Any {
+    val mensaje = "Los numeros son iguales"
      if (primerNumero > segundoNumero) {
          return (primerNumero-segundoNumero)-1
     }else if(primerNumero < segundoNumero) {
          return (segundoNumero-primerNumero)-1
     }else{
-         return error("Los numeros son iguales")
+         return mensaje
     }
 }
