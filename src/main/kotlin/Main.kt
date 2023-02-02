dd64d60109cf230275c6c027aebf3333476107b2
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-02-02 11:12:53 -0500

    ejercicio_6_calcule_suma_de_los_cuadrados_de_los_N_primeros_numeros

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-
+    var primerNumero = 0
+    tituloPrincipal()
+    tituloPrimerNumero()
+    primerNumero = readln().toInt()
+    println(
+        "La suma de los los cuadrados de los $primerNumero primeros numeros son --->>> ${
+            calcularSumaDeCuadrados(
+                primerNumero = primerNumero
+            )
+        }"
+    )
 }
