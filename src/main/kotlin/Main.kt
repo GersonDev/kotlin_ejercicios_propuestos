8f74b521f27ff849faf2661854a148b199969997
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-02-22 14:30:24 -0500

    ejercicio_28_bucles_calcular_el_factorial_de_un_número

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-
+    var primerNumero = 0
+    tituloPrincipal()
+    tituloIngreseNumero()
+    primerNumero = readLine()!!.toInt()
+    for (i in  primerNumero-1 downTo  1){
+        primerNumero *= i
+    }
+    println("Su factorial es -> $primerNumero")
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,6 @@
+fun tituloPrincipal(){
+    println("Hacer un programa que nos permita ingresar un numero y calcule su factorial")
+}
+fun tituloIngreseNumero(){
+    println("Ingrese numero que desee su factorial:")
+}
