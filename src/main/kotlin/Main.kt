4872622b837c034c56c9b59a4a3caece25536f23
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-02-22 11:14:30 -0500

    ejercicio_26_bucles_ingresar_10_números_y_calcular_la_suma

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-
+    var numeros: Int
+    var suma: Int = 0
+    tituloPrincipal()
+    for (i in 1..10) {
+        println("Ingrese el nuero $i:")
+        numeros = readln()!!.toInt()
+        suma = suma + numeros
+    }
+    println("la suma es:$suma")
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,4 @@
+
+fun tituloPrincipal(){
+    println("Construir un programa que permita ingresar 10 numeros y dar como resultado la suma total")
+}
