af6b7add636041be075213b43c45c6cc1b3da8c4
Gerson Cornelio <gersoncornelio1.gmail.com>
2023-02-22 14:16:34 -0500

    ejercicio_29_bucles_calcular_el_total_de_números_ingresados_por_el_usuario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-
+    var numero = 5
+    var contador = 0
+    tituloPrincipal()
+    while (numero != 0){
+        println("Ingrese un numero:")
+        numero = readLine()!!.toInt()
+        if(numero !=0){
+            contador = contador+1
+        }
+    }
+    println("Resultado esperado es: $contador")
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,3 @@
+fun tituloPrincipal() {
+    println("Dados los numeros ingresado imprimir cuantas veces el cliente utilizo el input y que se detenga cuando ingresen 0 ejemplo: 34,21,6,2,0 ->resultado esperado 4")
+}
